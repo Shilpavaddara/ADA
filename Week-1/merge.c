ad8d2f61735535eb949ad411d868624260cbacae
Shilpavaddara <shilpa.cs25@bmsce.ac.in>
2026-04-29 17:29:36 +0530

    Add files via upload

---
+++ Week-1/merge.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void merge(int arr[], int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    i = 0; j = 0; k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+
+    while (i < n1)
+        arr[k++] = L[i++];
+    while (j < n2)
+        arr[k++] = R[j++];
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    int n, i;
+    clock_t start, end;
+    double cpu_time;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    start = clock();
+    mergeSort(arr, 0, n - 1);
+    end = clock();
+
+    cpu_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    printf("\nSorted elements:\n");
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    printf("\n\nTime taken: %f seconds\n", cpu_time);
+
+    return 0;
+}
